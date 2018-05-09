98b0b2f8361032f4d8fd65b17c883cfda5b95d93
tytusw <tytus.wilczewski@gmail.com>
2018-05-09 13:25:21 +0200

    Problem 15 solved

--- problem_015.py
+++ problem_015.py
@@ -23,12 +23,29 @@ Solution :
 13x13 => 10400600
 14x14 => 134209536
 
-Question : combien de chemin a un 3x1 ou un 10x1, 10x2 etc?
+20x20 => 137846528820 -> Pascal s triangle => 40! / (20!)²
+
+All what follow is over-kill
 """
+def fill_pascal_triangle(val):
+    i = len(val)
+    j = len(val[0])
+    x =  1
+    y =  1
+
+    while x < i and y < j :
+        val[x][y] = val[x-1][y] + val[x][y-1]
+        x = x + 1
+        y = y + 1
+    print(val)
+
+move_right = 5 #right = logic 1
+move_down = move_right #down = logic 0, move_down = move_right if  if the grid is symetric
+tab = [ [ 1 for y in range( move_down ) ] for x in range( move_right ) ]
+fill_pascal_triangle(tab)
 
 
-move_right = 3 #right = logic 1
-move_down = 3 #down = logic 0, move_down = move_right if  if the grid is symetric
+"""
 bit_nbr = move_down
 
 val_min = 0
@@ -36,16 +53,16 @@ val_min = 0
 bit_cnt = 0
 way_cnt = 0
 
-val_min = 2**move_right - 1 #e.g. => val_min = 0b00011
-val_max = val_min << move_down #e.g. => val_max = 0b11000
+val_min = 2**move_right - 1 #e.g. => val_min = 0b0011
+val_max = val_min << move_down #e.g. => val_max = 0b1100
 
 
 print("for a",move_right,"x",move_down)
-print("val min =",bin(val_min))
+print("val min =",bin(val_min)[2:].zfill(move_right + move_down))
 #print("val min =",val_min)
-print("val max =",bin(val_max))
-#print("val max =",val_max)
-#il faut revoir l'algo de comptage
+print("val max =",bin(val_max)[2:].zfill(move_right + move_down))
+print("val max =",val_max)
+
 for i in range(val_min,val_max + 1):
     temp = i
     bit_cnt = 0
@@ -58,6 +75,8 @@ for i in range(val_min,val_max + 1):
         #print(i,"=>",bin(i)[2:].zfill(move_right + move_down))
 
 print("nbr of way =",way_cnt)
+"""
+
 
 
 
